ce339f590d6d2e164fc35a055e5da60e831c4630
seema patil <seemampatil22@gmail.com>
2025-03-15 14:46:39 +0530

    Add files via upload
    Square Number Pattern

---
+++ squarenum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class squarenum {
+    
+   public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                
+                System.out.print(j+"");
+            }
+            
+        
+            System.out.println();
+        }
+       
+    }   
+}
