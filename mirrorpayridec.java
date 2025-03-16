24aa593974529f1331ec60472aaaad1c40a173cf
seema patil <seemampatil22@gmail.com>
2025-03-16 11:31:27 +0530

    Add files via upload
    Centered Mirrored Decreasing Number Pyramid

---
+++ mirrorpayridec.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class mirrorpayridec {
+  
+        public static void main(String[] args) {
+             Scanner s=new Scanner(System.in);
+          System.out.println("enter a number");
+          int n=s.nextInt();
+    
+            for (int i = 1; i <= n; i++) {
+                
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                
+                for (int j = (n - i + 1); j <= n; j++) {
+                    System.out.print(j);
+                }
+             
+                for (int j = (n - 1); j >= (n - i + 1); j--) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+    }
+   
+    
+
