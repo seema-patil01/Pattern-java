2965158c6b94ed3fd7718534b6d6c9610bf698b7
seema patil <seemampatil22@gmail.com>
2025-05-07 18:19:08 +0530

    Add files via upload
    mixed linear

---
+++ mixdlinear.java
@@ -0,0 +1,17 @@
+public class mixdlinear {
+   
+        public static void main(String[] args) {
+            for (int i = 1; i <= 4; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            System.out.println(5);
+            System.out.println(6);
+            for (int i = 10; i >= 7; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+
