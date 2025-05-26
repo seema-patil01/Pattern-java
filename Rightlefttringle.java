e467f2aea766a43d9e1e52ee58bca3510dbb331c
seema patil <seemampatil22@gmail.com>
2025-05-26 12:35:55 +0530

    Add files via upload
    Right -Left Tringle pattern

---
+++ Rightlefttringle.java
@@ -0,0 +1,21 @@
+public class Rightlefttringle {
+   
+    public static void main(String[] args) {
+        int rows = 5;
+         for (int i = 1; i <= rows; i++) {
+           
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j < rows; j++) {
+                System.out.print("  ");
+            }
+             for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(); 
+        }
+    }
+}
+
