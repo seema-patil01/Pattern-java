263de54bae943d2de4b66b1312408bda6573e648
seema patil <seemampatil22@gmail.com>
2025-07-06 14:45:24 +0530

    Add files via upload
    Print N pattern

---
+++ Print_N.java
@@ -0,0 +1,20 @@
+public class Print_N {
+   
+    public static void main(String[] args) {
+        int n = 7; 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                if (j == 0 || j == n - 1 || i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+
