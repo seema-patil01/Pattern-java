f50393b01ee2b510fa3657c3162a319590a43dfc
seema patil <seemampatil22@gmail.com>
2025-07-09 10:36:48 +0530

    Add files via upload
    Print R pattern

---
+++ Print_R.java
@@ -0,0 +1,33 @@
+public class Print_R {
+    
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+               
+                if (j == 0) {
+                    System.out.print("*");
+                }
+                
+                else if ((i == 0 || i == n / 2) && j < n - 1) {
+                    System.out.print("*");
+                }
+          
+                else if (j == n - 1 && i > 0 && i < n / 2) {
+                    System.out.print("*");
+                }
+               
+                else if (i - j == n / 2) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
